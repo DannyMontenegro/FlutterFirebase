dd95fbc3854d365387ab66dfd0cc2a30b0638a7f
DannyMontenegro <djmonten@espol.edu.ec>
2022-03-30 11:14:17 -0500

    Auth service for email login

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -60,10 +60,19 @@ class _LoginViewState extends State<LoginView> {
                     email: email,
                     password: password,
                   );
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (route) => false,
-                  );
+
+                  final user = FirebaseAuth.instance.currentUser;
+                  if (user?.emailVerified ?? false) {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      notesRoute,
+                      (route) => false,
+                    );
+                  } else {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute,
+                      (route) => false,
+                    );
+                  }
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'user-not-found') {
                     await showErrorDialog(
@@ -84,12 +93,12 @@ class _LoginViewState extends State<LoginView> {
                       "Error ocurred",
                     );
                   }
-                } catch (e){
+                } catch (e) {
                   await showErrorDialog(
-                      context,
-                      e.toString(),
-                      "Something unexpected happened",
-                    );
+                    context,
+                    e.toString(),
+                    "Something unexpected happened",
+                  );
                 }
               },
               child: const Text("Login")),
@@ -108,5 +117,3 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
-
-
